35c99c86448c805c6712bd3eac8e58ebbb4d151d
yamija <jannitayamila@gmail.com>
2018-06-19 18:31:31 -0500

    Se direccionó el archivo de Stylos.css

--- src/data.js
+++ src/data.js
@@ -1,1 +1,21 @@
 
+
+let urlUser = "../data/cohorts/lim-2018-03-pre-core-pw/users.json";
+let urlProgress = "../data/cohorts/lim-2018-03-pre-core-pw/progress.json";
+
+
+function conexion(url) {
+    let xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function () {
+
+        if (this.readyState == 4 && this.status == 200) {
+            let arrayConexion = JSON.parse(this.responseText);
+            return arrayConexion;
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+
+
+}
