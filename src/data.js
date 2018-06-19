9c55e8405820da6ac5c77e47a0c880b8f9fb16ac
yamija <jannitayamila@gmail.com>
2018-06-19 18:50:14 -0500

    se agregó la function conexion a data.js

--- src/data.js
+++ src/data.js
@@ -4,18 +4,24 @@ let urlUser = "../data/cohorts/lim-2018-03-pre-core-pw/users.json";
 let urlProgress = "../data/cohorts/lim-2018-03-pre-core-pw/progress.json";
 
 
-function conexion(url) {
+function conexion(urlUser) {
     let xmlhttp = new XMLHttpRequest();
 
     xmlhttp.onreadystatechange = function () {
 
         if (this.readyState == 4 && this.status == 200) {
-            let arrayConexion = JSON.parse(this.responseText);
-            return arrayConexion;
+            let arrayDateJson = JSON.parse(this.responseText);
+          
+          
         }
     };
-    xmlhttp.open("GET", url, true);
+    xmlhttp.open("GET", urlUser, true);
     xmlhttp.send();
+  
 
+ 
+    for (let valor of arrayDateJson) {
+        console.log(arrayDateJson);
+    }
 
 }
